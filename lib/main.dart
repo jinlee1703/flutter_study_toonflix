87e254d273cdb8d9de7686490693e47bed9ee26e
Jinwoo Lee <binarywooo@gmail.com>
2023-01-31 19:12:45 +0900

    feat: implement cards from chapter3

--- lib/main.dart
+++ lib/main.dart
@@ -1,85 +1,128 @@
 import 'package:flutter/material.dart';
+import 'package:toonflix/widgets/button.dart';
+import 'package:toonflix/widgets/currency_card.dart';
 
 void main() {
   runApp(App());
 }
+
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Color(0xFF181818),
-        body:
-            Padding(padding: EdgeInsets.symmetric(horizontal: 40),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-              SizedBox(height: 80,),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.end,
-          children: [
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.end,
-              children: [
-                Text('Hey, Selena',
+        backgroundColor: const Color(0xFF181818),
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(
+                height: 80,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      const Text('Hey, Selena',
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 28,
+                              fontWeight: FontWeight.w800)),
+                      const Text('Welcome back',
+                          style: TextStyle(
+                              color: Color.fromRGBO(255, 255, 255, 0.8),
+                              fontSize: 18)),
+                    ],
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 50,
+              ),
+              Text('Total Balance',
+                  style: TextStyle(
+                      fontSize: 22, color: Colors.white.withOpacity(0.8))),
+              const SizedBox(
+                height: 10,
+              ),
+              const Text('\$5 194 482',
+                  style: TextStyle(
+                      fontSize: 44,
+                      fontWeight: FontWeight.w600,
+                      color: Colors.white)),
+              const SizedBox(
+                height: 30,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Button(
+                      text: 'Transfer',
+                      bgColor: const Color(0xFFF1B343),
+                      textColor: Colors.black),
+                  Button(
+                      text: 'Request',
+                      bgColor: const Color(0xFF1F2123),
+                      textColor: Colors.white),
+                ],
+              ),
+              const SizedBox(
+                height: 50,
+              ),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text('Wallets',
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 36,
+                          fontWeight: FontWeight.w600)),
+                  Text(
+                    'View All',
                     style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 28,
-                        fontWeight: FontWeight.w800
-                    )
-                ),
-                Text('Welcome back',
-                    style: TextStyle(
-                        color: Color.fromRGBO(255, 255, 255, 0.8),
-                        fontSize: 18
-                    )
-                ),
-              ],
-            ),
-
-          ],
-        ),
-        SizedBox(height: 120,),
-        Text(
-            'Total Balance',
-            style: TextStyle(
-                fontSize: 22,
-                color: Colors.white.withOpacity(0.8)
-            )
-        ),
-        SizedBox(height: 10,),
-      Text(
-          '\$5 194 482',
-          style: TextStyle(
-              fontSize: 44,
-              fontWeight: FontWeight.w600,
-              color: Colors.white
-          )
-      ),
-                SizedBox(height: 30,),
-                Row(children: [
-                  Container(
-                    decoration: BoxDecoration(
-                      color: Colors.amber,
-                      borderRadius: BorderRadius.circular(45)
+                      color: Colors.white.withOpacity(0.8),
+                      fontSize: 18,
                     ),
-                    child: Padding(
-                      padding: EdgeInsets.symmetric(
-                        vertical: 20,
-                        horizontal: 50,
-                      ),
-                      child: Text(
-                          'Transfer',
-                        style: TextStyle(
-                          fontSize: 18,
-                        ),
-
-                      ),
-                    ))
-                ],)
-        ],
-      ),),
-
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              CurrencyCard(
+                name: 'Euro',
+                code: 'EUR',
+                amount: '6 428',
+                icon: Icons.euro_rounded,
+                isInverted: false,
+              ),
+              Transform.translate(
+                offset: Offset(0, -20),
+                child: CurrencyCard(
+                  name: 'Bitcoin',
+                  code: 'BTC',
+                  amount: '9 785',
+                  icon: Icons.currency_bitcoin,
+                  isInverted: true,
+                ),
+              ),
+              Transform.translate(
+                offset: Offset(0, -20),
+                child: CurrencyCard(
+                  name: 'Dollar',
+                  code: 'USD',
+                  amount: '428',
+                  icon: Icons.attach_money_outlined,
+                  isInverted: false,
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
